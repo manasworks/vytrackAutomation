ca0e3d0d2ff62d44a5bb6064b64a078e7dfde506
Manas Kalenov <manasworks@gmail.com>
2022-03-06 02:55:57 -0600

    CY19_67

--- src/test/java/com/vytrack/pages/VehicleModel.java
+++ src/test/java/com/vytrack/pages/VehicleModel.java
@@ -13,7 +13,7 @@ public class VehicleModel {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy (xpath = "//th/*/span[1]")
+    @FindBy (xpath = "//th//span[1]")
     public List<WebElement> headerCells;
 
     @FindBy (xpath = "//td[@class='string-cell grid-cell grid-body-cell grid-body-cell-Vendors']")
