8b0b6b73756f88bf45a9b6f734d39a7a016c1caf
Manas Kalenov <manasworks@gmail.com>
2022-03-06 23:16:37 -0600

    CY19_64

--- src/test/java/com/vytrack/tests/CY19_64.java
+++ src/test/java/com/vytrack/tests/CY19_64.java
@@ -3,8 +3,6 @@ package com.vytrack.tests;
 import com.vytrack.pages.TopMenu;
 import com.vytrack.utilities.Driver;
 import com.vytrack.utilities.VytrackUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
