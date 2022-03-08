c5415e45ac1c5891fdc5a242c1810b369aa9cc01
Manas Kalenov <manasworks@gmail.com>
2022-03-08 09:46:36 -0600

    TestNG Suite

--- src/test/java/com/vytrack/pages/Accounts.java
+++ src/test/java/com/vytrack/pages/Accounts.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Accounts {
